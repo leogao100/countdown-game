a0058eaf6f7123ee7e032b0a5b7ab150eda889e5
Leo Gao (XIXISMILE) <130561834+leogao100@users.noreply.github.com>
2024-07-29 12:54:21 +0000

    frist commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className='bg-blue-500'>
+      <h1 className="text-amber-500">Hello, Vite + React + TypeScript + Tailwind!</h1>
+    </div>
+  );
+};
+
+export default App;
